af1eeffe0cfd7cda083cb2fa8ab1eac35b397776
nirmalrv8 <nirmal.seneviratne@gmail.com>
2024-10-26 22:14:15 +1300

    Change font colors.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,7 +11,11 @@ import {
 const AuthModal = () => {
   return (
     <Box
-      sx={{ width: '100vw', height: '100vh', background: 'linear-gradient(120deg, #4b9a8f, #5a8db6)',
+      sx={{ width: '100vw', height: '100vh',
+        backgroundImage: 'url("space-wallpaper-stars-background.gif")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
         display: 'flex', justifyContent: 'center', alignItems: 'center' }}
     >
       <Box sx={{ width: '600px', padding: '30px', backgroundColor: 'white', borderRadius: '20px', boxShadow: 3 }}>
@@ -27,14 +31,15 @@ const AuthModal = () => {
         {/* Sign Up and Login */}
         <Box display="flex" justifyContent="center" mt={3}>
           <Box width="50%">
-            <Typography variant="h6" align="center" gutterBottom sx={{ fontWeight: 500, marginBottom: '20px' }}>
+            <Typography variant="h6" align="center" gutterBottom
+              sx={{ fontWeight: 500, marginBottom: '20px', color: '#424547'}}>
               Sign up
             </Typography>
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }} >
               <Button variant="outlined"
                 startIcon={ <img src="Google-Symbol.png" alt="Google" style={{ width: 20, height: 20 }}/> }
-                sx={{ marginBottom: '18px', width: '240px', height: '40px' }}
+                sx={{ marginBottom: '18px', width: '240px', height: '40px', color: '#696d70', borderColor: '#696d70' }}
               >
                 Continue with Google
               </Button>
@@ -44,14 +49,15 @@ const AuthModal = () => {
               <Button
                 variant="outlined"
                 startIcon={ <img src="Facebook-logo.png" alt="Facebook" style={{ width: 32, height: 20 }} /> }
-                sx={{ marginBottom: '18px', width: '240px', height: '40px' }}
+                sx={{ marginBottom: '18px', width: '240px', height: '40px', color: '#696d70', borderColor: '#696d70' }}
               >
                 Continue with Facebook
               </Button>
             </Box>
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }} >
-              <Button variant="outlined" sx={{ marginBottom: '12px', width: '240px', height: '40px' }}
+              <Button variant="outlined"
+                sx={{ marginBottom: '12px', width: '240px', height: '40px', color: '#696d70', borderColor: '#696d70' }}
                 startIcon={ <img src="email-icon.png" alt="Email" style={{ width: 18, height: 18 }}/> }
               >
                 Sign up with email
@@ -68,13 +74,14 @@ const AuthModal = () => {
           <Divider orientation="vertical" flexItem sx={{ mx: 2 }} />
 
           <Box width="50%">
-            <Typography variant="h6" align="center" gutterBottom sx={{ fontWeight: 500, marginBottom: '20px' }}>
+            <Typography variant="h6" align="center" gutterBottom
+              sx={{ fontWeight: 500, marginBottom: '20px', color: '#424547' }}>
               Log in
             </Typography>
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
               <TextField label="Email address" type="email" variant="outlined"
-                sx={{ width: '220px', height: '40px', marginBottom: '20px',
+                sx={{ width: '240px', height: '40px', marginBottom: '20px',
                   '& .MuiOutlinedInput-root': { height: '40px' },
                   '& .MuiInputLabel-root': { lineHeight: '0.8', fontSize: '14px' },
                 }}
@@ -83,16 +90,19 @@ const AuthModal = () => {
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
               <TextField label="Password" type="password" variant="outlined"
-                sx={{ width: '220px', height: '40px', marginBottom: '10px',
+                sx={{ width: '240px', height: '40px', marginBottom: '10px',
                   '& .MuiOutlinedInput-root': { height: '40px' },
                   '& .MuiInputLabel-root': { lineHeight: '0.8', fontSize: '14px' },
                 }}
               />
             </Box>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', }}>
+              <Typography variant="caption" sx={{ display: 'flex', justifyContent: 'end', width: '240px' }}>
+                <a href="#" style={{ color: '#70757a',}}>Forgot your password?</a>
+              </Typography>
+            </Box>
             
-            <Typography variant="caption" align="right" display="block">
-              <a href="#" style={{ color: '#70757a' }}>Forgot your password?</a>
-            </Typography>
             <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '12px' }}>
               <Button variant="contained" color="primary" sx={{ color: 'white', width: '100px', height: '40px' }}>
                 Log in
